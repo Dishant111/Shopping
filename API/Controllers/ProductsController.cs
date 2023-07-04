ce237fbf8c706d461281ac06c23411a83e52aee6
Dishant Rana <ranadishant111@gmail.com>
2023-07-05 00:22:41 +0530

    Initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+
+        public ProductsController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> GetProductsAsync() 
+        {
+            return await _storeContext.Product.ToListAsync();
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Shopping
+ Udemy course
